12dec638d0a308ea7664e968282c28b7ebf3407a
fabiotorresdev <fabiodev2022@gmail.com>
2022-07-04 20:28:03 -0300

    inicio js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 document.querySelector('.botao-pause').style.display = 'none'
+
+
+let musica = document.querySelector('audio');
+
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+function tocarMusica(){
+    musica.play();
+}
