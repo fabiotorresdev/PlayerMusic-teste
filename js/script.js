1ccd10bdd053103a82c611c8eacc803f3a8b01b9
fabiotorresdev <fabiodev2022@gmail.com>
2022-07-10 00:18:40 -0300

    atualizando player

--- js/script.js
+++ js/script.js
@@ -3,8 +3,49 @@ document.querySelector('.botao-pause').style.display = 'none'
 
 let musica = document.querySelector('audio');
 
+let duracaoMusica = document.querySelector('.fim');
+
+duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+
+// Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
-function tocarMusica(){
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra)
+
+// Funções
+function tocarMusica() {
     musica.play();
+    document.querySelector('.botao-pause').style.display = 'block'
+    document.querySelector('.botao-play').style.display = 'none'
 }
+
+function pausarMusica() {
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none'
+    document.querySelector('.botao-play').style.display = 'block'
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function duration(){
+    duracaoMusica.textContent = segundosParaMinutos(Math.floor(currentSong.duration));
+}
+
+
+function segundosParaMinutos(segundos) {
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10) {
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
+}
+
